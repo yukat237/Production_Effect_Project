2e61de1fb4435d165f6f5a9c8c69677e134d5847
yukat237 <tatsumikan0723@gmail.com>
2023-05-07 19:27:27 -0400

    completed the second part

--- scoringprep.R
+++ scoringprep.R
@@ -18,25 +18,48 @@ write_csv(flipDf, paste0(dir,"cond_flipped.csv"))
 
 dirWL <- "/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist"
 
+# read in the realword table (word image file name & actual word stim comparison table)
+wordinfo <- read.xlsx("/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist_with_real_words/stimuli list for scoring.xlsx")
+
 #making a file name list
 setwd(dirWL)
 fileListWL <- list.files(dirWL)
 fileNum <- length(fileListWL)
 
+# prepare a dataframe for the full data
+fullDatWL <- data.frame(listname = character(0),
+                      filenum = character(0),
+                      realword = character(0))
+
 #read each file
 for (i in 1:fileNum){
   
   # import data
-  originalWL<-read.csv(fileListWL[i])
-  # leave only needed columns
-  dat<-data.frame(originalCSV$participant, originalCSV$session, originalCSV$textbox.text)
-  # rename columns
-  colnames(dat) <- c("ptID","session","resp")
-  # remove rows that are empty in resp column
-  dat <- dat[dat$resp != "",]
-  # write out this info
+  originalWL<-read.xlsx(fileListWL[i])
+  # leave only needed columns (column 1)
+  dat<- select(originalWL, wordStim)
+  # take only the numbers from this column
+  dat$wordStim <- str_extract(dat$wordStim, pattern = "w[:digit:]*")
+  dat$wordStim <- str_replace_all(dat$wordStim, "w", "")
+  dat <- dat %>% rename( "filenum" = "wordStim" )
+  
+  # add another column based on the wordinfo df
+  dat_w_realwords <- merge(dat, wordinfo, by = "filenum", sort = F)
+  
+  # make a new column name to represent listname (= filename)
+  dat_w_realwords$listname <- rep(fileListWL[i], times = 30)
+  dat_w_realwords$listname <- gsub(".xlsx", "", dat_w_realwords$listname)
+  dat_w_realwords <- dat_w_realwords[,c(3,1,2)]
+  
+  # append this to full df
   # "C:\Users\yuka\iCloudDrive\@midd\Middlebury Senior\Thesis2\wordlist_with_real_words"
-  fullDat<-rbind(fullDat,dat)  
+  fullDatWL<-rbind(fullDatWL,dat_w_realwords)
   
 }
+
+# set the output path
+pathOut <- "/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist_with_real_words/realwords.xlsx"
+
+# save the output
+write.table(fullDatWL, pathOut, row.names = F)
 
