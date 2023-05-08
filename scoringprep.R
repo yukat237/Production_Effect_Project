27b2f33b41c4a35576b7cfbc4c3724ea592def64
yukat237 <tatsumikan0723@gmail.com>
2023-05-07 20:54:07 -0400

    updated to keep sayStim column

--- scoringprep.R
+++ scoringprep.R
@@ -1,6 +1,6 @@
 ### to prep for the scoring process ###
 
-## obtain condition info
+## 1) obtain condition info------------------------------------------------------------
 dir <- "/Users/yuka/Desktop/Production_Effect_Project/"
 tgfilename<-"cond_info.csv"
 tgfilepath<- paste0(dir,tgfilename)
@@ -13,8 +13,7 @@ flipDf<- t(condinfoDF) %>% as.data.frame()
 write_csv(flipDf, paste0(dir,"cond_flipped.csv"))
 
 
-## process each wordlist to have actual words
-# "C:\Users\yuka\iCloudDrive\@midd\Middlebury Senior\Thesis2\wordlist"
+## 2) process each wordlist to have actual words------------------------------------------------------------
 
 dirWL <- "/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist"
 
@@ -29,15 +28,15 @@ fileNum <- length(fileListWL)
 # prepare a dataframe for the full data
 fullDatWL <- data.frame(listname = character(0),
                       filenum = character(0),
-                      realword = character(0))
+                      realword = character(0),
+                      sayStim = character(0))
 
 #read each file
 for (i in 1:fileNum){
   
   # import data
-  originalWL<-read.xlsx(fileListWL[i])
-  # leave only needed columns (column 1)
-  dat<- select(originalWL, wordStim)
+  dat<-read.xlsx(fileListWL[i])
+  
   # take only the numbers from this column
   dat$wordStim <- str_extract(dat$wordStim, pattern = "w[:digit:]*")
   dat$wordStim <- str_replace_all(dat$wordStim, "w", "")
@@ -58,8 +57,8 @@ for (i in 1:fileNum){
 }
 
 # set the output path
-pathOut <- "/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist_with_real_words/realwords.xlsx"
+pathOut <- "/Users/yuka/iCloudDrive/@midd/Middlebury Senior/Thesis2/wordlist_with_real_words/realwords.csv"
 
 # save the output
-write.table(fullDatWL, pathOut, row.names = F)
+write.table(fullDatWL, pathOut,  sep = ",", row.names = F)
 
